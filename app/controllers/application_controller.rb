05c54cefd7bb14826d2fc05e38c345216200d042
Luis Zabala <luis.zabala076@gmail.com>
2019-03-08 22:22:36 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -37,7 +37,7 @@ class ApplicationController < Sinatra::Base
 
   post "/login" do
     user = User.find_by(:username => params[:username])
-    if user.authentication
+    if user && user.authentication
   end
 
   get "/failure" do
