f1bf14a0de3f5c5c4bee31a7d6e45a7b33b807d3
Luis Zabala <luis.zabala076@gmail.com>
2019-03-08 22:24:04 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -38,7 +38,7 @@ class ApplicationController < Sinatra::Base
   post "/login" do
     user = User.find_by(:username => params[:username])
     if user && user.authentication(params[:password])
-      session(params[user])
+      session(params[:user_id]) == user.id
 
   end
 
